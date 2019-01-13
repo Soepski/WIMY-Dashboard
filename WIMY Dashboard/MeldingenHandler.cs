44e89d0c598e0c2326debec21317c3d83ef977cb
Ron Liebregts <ronlieb@hotmail.com>
2019-01-13 20:18:44 +0100

    code opgedeeld in klassen

---
+++ WIMY Dashboard/MeldingenHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WIMY_Dashboard
+{
+    class MeldingenHandler
+    {
+        Database db = new Database();
+        MainWindow mainwindow = new MainWindow();
+        SerialPort serial = new SerialPort("COM4", 9600);
+        public void Verwerk()
+        {
+            if (serial.IsOpen)
+            {
+                string SerialMelding = serial.ReadLine().ToString();
+                mainwindow.lvMeldingen.Items.Add($"{SerialMelding} {DateTime.Now.ToString()}");
+                using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"C:\Users\Public\LogFile.txt", true))
+                {
+                    file.WriteLine(SerialMelding + $" {DateTime.Now.ToString()}");
+                    file.WriteLine("");
+                }
+
+                //Melding moet nog naar database
+            }
+            else
+            {
+                serial.Open();
+            }
+        }
+    }
+}
